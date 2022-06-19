f6ed795e6b848db5b378dc96f4183605438790d1
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-06-19 20:28:34 +0400

    3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-			React + Reduxc
+			React + Redux
         </p>
         <a
           className="App-link"

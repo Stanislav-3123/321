66b82aff37556b989efa64f15864fb171e9e4a8e
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-06-28 21:19:42 +0400

    Libraries

---
+++ to-do-list/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<div className="container">
+				<h1>Test</h1>
+			</div>
+		</BrowserRouter>
+	);
+}
+
+export default App;

8c824716ecbcd9c949324ffa46491e823cd7cbae
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-06-29 13:31:25 +0400

    Черновик для роутера

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -14,3 +14,29 @@ function App() {
 }
 
 export default App;
+// import React from 'react'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import { Home } from './pages/Home'
+// import { About } from './pages/About'
+// import { Navbar } from './components/Navbar';
+// import { Alert } from './components/Alert';
+// import { AlertState } from './context/alert/AlertState';
+
+// function App() {
+//     return (
+//       <AlertState>
+//       <Router>
+//           <Navbar />
+//         <div className="container pt-4">
+//             <Alert />
+//           <Routes>
+//           <Route path={'/'} exact element={<Home/>} />
+//           <Route path={'/about'} element={<About/>} />
+//           </Routes>
+//         </div>
+//       </Router>
+//       </AlertState>
+//     );
+//   }
+
+// export default App;

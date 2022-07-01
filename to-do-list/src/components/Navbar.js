1e21b16306d7c5761d47ce97962f276cf4490521
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-07-01 18:51:25 +0400

    ul

--- to-do-list/src/components/Navbar.js
+++ to-do-list/src/components/Navbar.js
@@ -7,9 +7,21 @@ export const Navbar = () => (
 			Note App
 		</div>
 		<ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink className="nav-link" href="#">Home <span className="sr-only">(current)</span></NavLink>
-        </li>
-      </ul>
+			<li className="nav-item">
+				<NavLink
+					className="nav-link"
+					to="/"
+				>Главная
+				</NavLink>
+			</li>
+			<li className="nav-item">
+				<NavLink
+					className="nav-link"
+					to="/about"
+				>Информация
+				</NavLink>
+			</li>
+
+		</ul>
 	</nav>
 )
